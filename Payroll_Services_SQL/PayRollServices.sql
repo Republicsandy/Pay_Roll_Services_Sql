37d6b065f7500b0a929732bacbbd225a7cb0bb60
Sandeep singh <sandy180388@gmail.com>
2022-10-03 17:55:09 +0530

    [sandeep] added file retrive Specific data to UC_%

--- Payroll_Services_SQL/PayRollServices.sql
+++ Payroll_Services_SQL/PayRollServices.sql
@@ -1,17 +1,22 @@
-﻿-----UC-1 Create DataBase-----
-create database pay_roll_service
-use pay_roll_service
+﻿
+-----UC-1 Create DataBase-----
+create database payroll_service
+use payroll_service
 -----UC-2 Create table for DataBase payroll-----
-create table Employee_Pay_Roll(			
-EmployeeId int IDENTITY(1,1) primary key,			
-NAME varchar(255),			
-Salary float,			
-StartData Date);
------------------------UC - 3 -----------add employe into table- -----			
-INSERT INTO Employee_Pay_Roll (Name,Salary,StartData)			
-VALUES ('Dhoni',68125.23,getDate());			
-			
-INSERT INTO Employee_Pay_Roll			
-VALUES ('UV',38423.32,getDate());			
-			-----UC-4 Retrieve all data from the table-----
-select * from Employee_Pay_Roll	
+create table employee_payroll(
+EmployeeId int identity(1,1) primary key,
+EmployeeName varchar(255),
+EmployeeSalary float,
+StartDate Date
+)
+-----UC-3 Insert Values to table-----
+Insert into employee_payroll
+(EmployeeName,EmployeeSalary,StartDate)
+values('Ram Kumar','650000','2020-10-07'),('Vijay','700000','2019-05-08'),('Priya','350500','2021-01-17');
+
+Insert into employee_payroll(EmployeeName,EmployeeSalary,StartDate)values('Asif','950500','2017-12-12');
+-----UC-4 Retrieve all data from the table-----
+select * from employee_payroll;
+-----UC-5 Retrieve Specific Data-----
+select EmployeeSalary from employee_payroll where EmployeeName = 'Vijay';
+select EmployeeSalary from employee_payroll where StartDate Between Cast('2019-01-01' as Date) and GETDATE();
--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -9,23 +9,23 @@ use PayRollService
 -----------------UC-2-create table---------------			
 drop table EmployeePayRoll;			
 			
-CREATE TABLE EmployeePayRoll(			
+CREATE TABLE Employee_Pay_Roll(			
 EmployeeId int IDENTITY(1,1) primary key,			
 NAME varchar(255),			
 Salary float,			
 StartData Date);			
 			
 -----------------------UC - 3 -----------add employe into table- -----			
-INSERT INTO EmployeePayRoll (Name,Salary,StartData)			
+INSERT INTO Employee_Pay_Roll (Name,Salary,StartData)			
 VALUES ('Dhoni',68125.23,getDate());			
 			
-INSERT INTO EmployeePayRoll			
+INSERT INTO Employee_Pay_Roll			
 VALUES ('UV',38423.32,getDate());			
 			
-select * from EmployeePayRoll			
+select * from Employee_Pay_Roll			
 ----------------UC-3_ read data from table-------------------			
-select * from EmployeePayRoll where Name='Dhoni'			
+select * from Employee_Pay_Roll where Name='Dhoni'			
 			
-INSERT INTO EmployeePayRoll (Name,Salary,StartData)			
+INSERT INTO Employee_Pay_Roll (Name,Salary,StartData)			
 VALUES ('Kumar',48125.23,'2019-02-03'),('kumar',40125.23,'2009-12-03'),('kalpana',73242.1,'1991-05-06');			
 			
